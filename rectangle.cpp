cf1f0beed2334b1a8d8faa88b0281ea7ac16ce75
programmios <127547151+programmios@users.noreply.github.com>
2023-11-14 09:49:11 +0330

    initial rectangle

---
+++ rectangle.cpp
@@ -0,0 +1,33 @@
+// in mainwindow.cpp
+#include "QGraphicsScene"
+#include "QGraphicsView"
+#include "QBrush"
+#include "QPen"
+
+scene = new QGraphicsScene(this);
+ui->graphicsView->setScene(scene);
+QBrush brush(Qt::red);
+QPen pen(Qt::black);
+pen.setWidth(5);                                    // ضحامت بردر رو مشخص میکنه
+rect = scene->addRect(20, 20, 100, 50, pen, brush); // 20,20 مختصات گوشه ی بالا سمت چپ مستطیله
+                                                    // 100,50 مختصات گوشه ی سمت راست پایین مستطیله
+                                                    // pen = border of rectangle = black
+                                                    // brush = color of rectangle = red
+                                                    // انتخاب شده بود ui که ابتدای کار از GraphicsView نیاز داریم که یک صحته ی گرافیکی رو برای ما ایجاد کنه(GraphicsScene) ، scene به یک addRect برای
+                                                    // ما scene میشه با متغیر setScene که
+rect->setFlag(QGraphicsItem::ItemIsMovable);
+void MainWindow::on_pushButton_clicked()
+{
+    ui->graphicsView->rotate(45);
+}
+
+// in mainwindow.h
+
+#include "QGraphicsScene"
+#include "QGraphicsRectItem"
+
+private:
+Ui::MainWindow *ui;
+QGraphicsScene scene;
+
+// in mainwindow.cpp
