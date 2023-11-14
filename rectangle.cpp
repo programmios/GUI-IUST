fce7b042ea984da6b1aef6c809cf804ed2cf11be
programmios <127547151+programmios@users.noreply.github.com>
2023-11-14 10:17:34 +0330

    Update rectangle.cpp

--- rectangle.cpp
+++ rectangle.cpp
@@ -9,8 +9,8 @@ ui->graphicsView->setScene(scene);
 QBrush brush(Qt::red);
 QPen pen(Qt::black);
 pen.setWidth(5);                                    // ضحامت بردر رو مشخص میکنه
-rect = scene->addRect(20, 20, 100, 50, pen, brush); // 20,20 مختصات گوشه ی بالا سمت چپ مستطیله
-                                                    // 100,50 مختصات گوشه ی سمت راست پایین مستطیله
+rect = scene->addRect(20, 20, 100, 50, pen, brush); // 20,20 گوشه ی بالایی سمت چپ رو به ما میده
+                                                    //  و 100,50 هم عرض و ارتفاع مستطیل ماست
                                                     // pen = border of rectangle = black
                                                     // brush = color of rectangle = red
                                                     // انتخاب شده بود ui که ابتدای کار از GraphicsView نیاز داریم که یک صحته ی گرافیکی رو برای ما ایجاد کنه(GraphicsScene) ، scene به یک addRect برای
